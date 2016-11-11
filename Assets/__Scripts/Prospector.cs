3b550abcbb15e37fc4ae276fc98e7b2ee687c62d
MichaelAlbright <michaelalbright25@yahoo.com>
2016-11-11 14:23:08 -0500

    599

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -10,19 +10,92 @@ public class Prospector : MonoBehaviour {
 
 	public Layout layout;
 	public TextAsset layoutXML;
+	public Vector3 layoutCenter;
+	public float xOffset = 3;
+	public float yOffset = -2.5f;
+	public Transform layoutAnchor;
+
+	public CardProspector target;
+	public List<CardProspector> tableau;
+	public List<CardProspector> discardPile;
 
 	void Awake ()
 	{
 		S = this;
 	}
 
+	public List<CardProspector> drawPile;
+
 	void Start ()
 	{
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
-		Deck.Shuffle (ref deck.cards);
+//		Deck.Shuffle (ref deck.cards);
 
 		layout = GetComponent<Layout> ();
 		layout.ReadLayout (layoutXML.text);
+
+		drawPile = ConvertListCardsToLostCardProspectors (deck.cards);
+		LayoutGame ();
+	}
+
+	CardProspector Draw()
+	{
+		CardProspector cd = drawPile [0];
+		drawPile.RemoveAt (0);
+		return(cd);
+	}
+
+	void LayoutGame()
+	{
+		if (layoutAnchor == null) {
+			GameObject tGO = new GameObject ("_LayoutAnchor");
+			layoutAnchor = tGO.transform;
+			layoutAnchor.transform.position = layoutCenter;
+		}
+
+		CardProspector cp;
+		foreach (SlotDef tSD in layout.slotDefs) {
+			cp = Draw ();
+			cp.faceUp = tSD.faceUp;
+			cp.transform.parent = layoutAnchor;
+			cp.transform.localPosition = new Vector3 (
+				layout.multiplier.x * tSD.x,
+				layout.multiplier.y * tSD.y,
+				-tSD.layerID);
+			cp.layoutID = tSD.id;
+			cp.slotDef = tSD;
+			cp.state = CardState.tableau;
+
+			cp.SetSortingLayerName (tSD.layerName);
+
+			tableau.Add (cp);
+		}
+	}
+
+	List<CardProspector> ConvertListCardsToLostCardProspectors(List<Card> lCD)
+	{
+		List<CardProspector> lCP = new List<CardProspector> ();
+		CardProspector tCP;
+		foreach (Card tCD in lCD) {
+			tCP = tCD as CardProspector;
+			lCP.Add (tCP);
+		}
+		return(lCP);
+	}
+
+	public void CardClicked(CardProspector cd)
+	{
+		switch (cd.state) {
+		case CardState.target:
+			break;
+		case CardState.drawpile:
+			MoveToDiscard (target);
+			MoveToTarget (Draw ());
+			UpdateDrawPile ();
+			break:
+		case CardState.tableau:
+			break;
+		}
 	}
 }
